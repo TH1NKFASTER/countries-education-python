68c539e7feb6d711d84ab3acc3d3e3ae62ddde6b
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2019-05-02 23:17:04 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,30 +2,48 @@ import flask
 import json
 import requests
 import argparse
-from Education import University
+from Education import University, IMO_participant
 
+# University.drop_table()
 University.create_table()
+new_university = University.create(rank_2019=1, rank_2018=1, university_name='MASSACHUSETTS INSTITUTE OF TECHNOLOGY', country_name='USA', alumni_employment_rank=4)
+new_university.save()
+new_university = University.create(rank_2019=2, rank_2018=2, university_name='Stanford University', country_name='USA', alumni_employment_rank=5)
+new_university.save()
+new_university = University.create(rank_2019=3, rank_2018=3, university_name='Harvard University', country_name='USA', alumni_employment_rank=1)
+new_university.save()
+new_university = University.create(rank_2019=4, rank_2018=4, university_name='Caltech', country_name='USA', alumni_employment_rank=72)
+new_university.save()
+new_university = University.create(rank_2019=5, rank_2018=6, university_name='University of Oxford', country_name='UK', alumni_employment_rank=3)
+new_university.save()
+new_university = University.create(rank_2019=312, rank_2018=355, university_name='MOSCOW INSTITUTE OF PHYSICS AND TECHNOLOGY', country_name='Russia', alumni_employment_rank=334)
+new_university.save()
 
-app = flask.Flask("University")
+# IMO_participant.drop_table()
+IMO_participant.create_table()
+new_participant = IMO_participant.create(year=1984, country='USS', firstname='Andrei', lastname='Astrelin', total=42, rank=1, award='Gold_medal')
+new_participant.save()
+new_participant = IMO_participant.create(year=1984, country='USS', firstname='Konstantin', lastname='Ignatiev', total=42, rank=1, award='Gold_medal')
+new_participant.save()
+new_participant = IMO_participant.create(year=1984, country='USS', firstname='Leonid', lastname='Oridoroga', total=42, rank=1, award='Gold_medal')
+new_participant.save()
+new_participant = IMO_participant.create(year=1984, country='USA', firstname='David', lastname='Moews', total=42, rank=1, award='Gold_medal')
+new_participant.save()
+new_participant = IMO_participant.create(year=1984, country='USS', firstname='Fedor', lastname='Nazarov', total=41, rank=9, award='Gold_medal')
+new_participant.save()
+
+app = flask.Flask("Education")
 app.university = University()
 common_uni = University()
+app.imo_participant = IMO_participant()
 
 
 @app.route('/add_university', methods=['POST'])
 def add_university():
-    # if 'university_name' in flask.request.args:
-    #     and 'rank_2018' in flask.request.args and 'university_name' in flask.request.args and 'country_name' in flask.request.args and 'academic_reputation_score' in flask.request.args and 'academic_reputation_rank' in flask.request.args and 'employer_reputation_score' in flask.request.args and 'employer_reputation_rank' in flask.request.args and 'faculty_student_score' in flask.request.args and 'faculty_student_rank' in flask.request.args and 'citations_per_faculty_score' in flask.request.args and 'citations_per_faculty_rank' in flask.request.args and 'international_faculty_score' in flask.request.args and 'international_faculty_rank' in flask.request.args and 'international_students_score' in flask.request.args and 'international_students_rank' in flask.request.args and 'overall_score' in flask.request.args:
-    #     print(app.university)
-    new_university = University.create(university_name=flask.request.args['university_name'])
+    new_university = University.create(university_name=flask.request.args['university_name'], rank_2019=flask.request.args['rank_2019']
+                                       , rank_2018=flask.request.args['rank_2018'], country_name=flask.request.args['country_name'],
+                                       alumni_employment_rank=flask.request.args['alumni_employment_rank'])
     new_university.save()
-    # , flask.request.args['rank_2018'], flask.request.args['university_name'], flask.request.args['country_name'],
-    # flask.request.args['academic_reputation_score'], flask.request.args['academic_reputation_rank'], flask.request.args['employer_reputation_score'],
-    # flask.request.args['employer_reputation_rank'], flask.request.args['faculty_student_score'], flask.request.args['faculty_student_rank'],
-    # flask.request.args['citations_per_faculty_score'], flask.request.args['citations_per_faculty_rank'], flask.request.args['international_faculty_score'],
-    # flask.request.args['international_faculty_rank'], flask.request.args['international_students_score'], flask.request.args['international_students_rank'],
-    # flask.request.args['overall_score'])
-    # if 'University' in flask.request.args:
-    #     flask.request.args['University'] .save()
 
 
 @app.route('/universities', methods=['GET'])
@@ -35,16 +53,34 @@ def universities():
 
 @app.route('/count_universities', methods=['GET'])
 def count():
-    return json.dumps(app.university.count()) + "\n"
+    return json.dumps(app.university.count()) + '\n'
+
+
+@app.route('/sort_universities_by_characteristic', methods=['GET'])
+def sort_universities_by_characteristic():
+    return json.dumps(app.university.sort_universities_by_characteristic(flask.request.args['characteristic'], flask.request.args['direction']))
+
+
+@app.route('/imo_participants', methods=['GET'])
+def imo_participants():
+    return json.dumps(app.imo_participant.show_all()) + '\n'
+
+
+@app.route('/add_imo_participant', methods=['POST'])
+def add_participant():
+    new_participant = IMO_participant.create(year=flask.request.args['year'], country=flask.request.args['country'], firstname=flask.request.args['firstname'], lastname=flask.request.args['lastname'],
+                                             total=flask.request.args['total'], rank=flask.request.args['rank'], award=flask.request.args['award'])
+    new_participant.save()
+
+
+@app.route('/count_participants', methods=['GET'])
+def count_partcipants():
+    return json.dumps(app.imo_participant.count()) + '\n'
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', default=50000, type=int)
-    # parser.add_argument('--db_name', default='postgres', type=str)
-    # parser.add_argument('--user', default='postgres', type=str)
-    # parser.add_argument('--password', default='1077', type=str)
-    # parser.add_argument('--host', default='localhost', type=str)
+    parser.add_argument('--host', default='localhost', type=str)
     args = parser.parse_args()
-
     app.run('::', args.port, debug=True, threaded=True)
