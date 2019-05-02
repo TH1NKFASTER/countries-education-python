dd880b19b7d7e39a2df3801b9b921aa1b51953ba
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2019-05-02 23:16:11 +0300

    Update Education.py

--- Education.py
+++ Education.py
@@ -1,59 +1,87 @@
 from peewee import *
 
-db = PostgresqlDatabase(database="postgres", user="postgres", password="1077", host='localhost')
+database = 'postgres'
+user = 'postgres'
+password = '1077'
+host = 'localhost'  # нормально без костылей не вышло
+db = PostgresqlDatabase(database=database, user=user, password=password, host=host)
 
 
-class University(Model):
-    # rank_2019 = IntegerField()
-    # rank_2018 = IdentityField()
-    university_name = CharField()
-
-    # country_name = CharField()
-    # academic_reputation_score = FloatField()
-    # academic_reputation_rank = IntegerField()
-    # employer_reputation_score = FloatField()
-    # employer_reputation_rank = IntegerField()
-    # faculty_student_score = FloatField()
-    # faculty_student_rank = IntegerField()
-    # citations_per_faculty_score = FloatField()
-    # citations_per_faculty_rank = IntegerField()
-    # international_faculty_score = FloatField()
-    # international_faculty_rank = IntegerField()
-    # international_students_score = FloatField()
-    # international_students_rank = IntegerField()
-    # overall_score = FloatField()
+class IMO_participant(Model):
+    year = IntegerField()
+    country = CharField()
+    firstname = CharField()
+    lastname = CharField()
+    total = IntegerField()
+    rank = IntegerField()
+    award = CharField()
 
     class Meta:
         database = db
 
-    def add_university(self, university_name):
-        # , rank_2018,  university_name, country_name, academic_reputation_score, academic_reputation_rank, employer_reputation_score, employer_reputation_rank,
-        #                faculty_student_score, faculty_student_rank, citations_per_faculty_score, citations_per_faculty_rank, international_faculty_score, international_faculty_rank,
-        #                international_students_score, international_students_rank, overall_score):
-        new_university = University.create(university_name=university_name)
-        # , rank_2018=rank_2018, university_name=university_name, country_name=country_name, academic_reputation_score=academic_reputation_score,
-        #                                academic_reputation_rank=academic_reputation_rank, employer_reputation_score=employer_reputation_score, employer_reputation_rank=employer_reputation_rank,
-        #                                faculty_student_score=faculty_student_score, faculty_student_rank=faculty_student_rank, citations_per_faculty_score=citations_per_faculty_score,
-        #                                citations_per_faculty_rank=citations_per_faculty_rank, international_faculty_score=international_faculty_score,
-        #                                international_faculty_rank=international_faculty_rank,
-        #                                international_students_score=international_students_score, international_students_rank=international_students_rank, overall_score=overall_score)
-        new_university.save()
+    def show_all(self):
+        y = list()
+        for participant in IMO_participant.select():
+            x = list()
+            x.append(participant.year)
+            x.append(participant.country)
+            x.append(participant.firstname)
+            x.append(participant.lastname)
+            x.append(participant.total)
+            x.append(participant.rank)
+            x.append(participant.award)
+            y.append(x)
+        return y
+
+    def count(self):
+        common_participant = IMO_participant()
+        return len(IMO_participant.show_all(common_participant))
+
+
+class University(Model):
+    rank_2019 = IntegerField()
+    rank_2018 = IntegerField()
+    university_name = CharField()
+    country_name = CharField()
+    alumni_employment_rank = IntegerField()
+
+    class Meta:
+        database = db
 
     def show_all(self):
-        x = list()
-        for uni in University.select(University.university_name):
+        y = list()
+        for uni in University.select():
+            x = list()
+            x.append(uni.rank_2019)
+            x.append(uni.rank_2018)
             x.append(uni.university_name)
-        return x
+            x.append(uni.country_name)
+            x.append(uni.alumni_employment_rank)
+            y.append(x)
+        return y
 
     def count(self):
         common_university = University()
         return len(University.show_all(common_university))
 
-# common_university = University()
-# University.drop_table()
-#
-# University.create_table()
-# University.add_university(common_university, 'MIPT')
-# University.add_university(common_university, 'Stanford')
-# print(University.show_all(common_university))
-# print(len(University.show_all(common_university)))
+    def sort_universities_by_characteristic(self, characteristic='rank_2019', direction='ASC'):
+        y = list()
+        if direction == 'ASC':
+            for uni in University.select().order_by(getattr(University, characteristic)):
+                x = list()
+                x.append(uni.rank_2019)
+                x.append(uni.rank_2018)
+                x.append(uni.university_name)
+                x.append(uni.country_name)
+                x.append(uni.alumni_emplyment_rank)
+                y.append(x)
+        elif direction == 'DESC':
+            for uni in University.select().order_by(-getattr(University, characteristic)):
+                x = list()
+                x.append(uni.rank_2019)
+                x.append(uni.rank_2018)
+                x.append(uni.university_name)
+                x.append(uni.country_name)
+                x.append(uni.alumni_emplyment_rank)
+                y.append(x)
+        return y
