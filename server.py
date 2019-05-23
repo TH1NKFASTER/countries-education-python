132f5741faab11fa6b54a717ec1c535b62b3d73d
TH1NKFASTER <46980911+TH1NKFASTER@users.noreply.github.com>
2019-05-23 20:35:31 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,11 @@
 import flask
 import json
-import argparse
 from Education import University, IMO_participant
 from constants import *
+import university_table
+import imo_participants_table
 
-University.create_table()
-# new_university = University.create(rank_2019=1, rank_2018=1, university_name='MASSACHUSETTS INSTITUTE OF TECHNOLOGY', country_name='USA', alumni_employment_rank=4)
-# new_university.save()
-# new_university = University.create(rank_2019=2, rank_2018=2, university_name='Stanford University', country_name='USA', alumni_employment_rank=5)
-# new_university.save()
-# new_university = University.create(rank_2019=3, rank_2018=3, university_name='Harvard University', country_name='USA', alumni_employment_rank=1)
-# new_university.save()
-# new_university = University.create(rank_2019=4, rank_2018=4, university_name='Caltech', country_name='USA', alumni_employment_rank=72)
-# new_university.save()
-# new_university = University.create(rank_2019=5, rank_2018=6, university_name='University of Oxford', country_name='UK', alumni_employment_rank=3)
-# new_university.save()
-# new_university = University.create(rank_2019=312, rank_2018=355, university_name='MOSCOW INSTITUTE OF PHYSICS AND TECHNOLOGY', country_name='Russia', alumni_employment_rank=334)
-# new_university.save()
-# University.drop_table()
-IMO_participant.create_table()
-# new_participant = IMO_participant.create(year=1984, country='USS', firstname='Andrei', lastname='Astrelin', rank=1, award='Gold_medal')
-# new_participant.save()
-# new_participant = IMO_participant.create(year=1984, country='USS', firstname='Konstantin', lastname='Ignatiev', rank=1, award='Gold_medal')
-# new_participant.save()
-# new_participant = IMO_participant.create(year=1984, country='USS', firstname='Leonid', lastname='Oridoroga', rank=1, award='Gold_medal')
-# new_participant.save()
-# new_participant = IMO_participant.create(year=1984, country='USA', firstname='David', lastname='Moews', rank=1, award='Gold_medal')
-# new_participant.save()
-# new_participant = IMO_participant.create(year=1984, country='USS', firstname='Fedor', lastname='Nazarov', rank=9, award='Gold_medal')
-# new_participant.save()
-# IMO_participant.drop_table()
+
 app = flask.Flask("Education")
 app.university = University()
 app.imo_participant = IMO_participant()
